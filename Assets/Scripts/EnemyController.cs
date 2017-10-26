3fe16052c063a94957306f805071b8926e09e225
gejimayu <gianffh@gmail.com>
2017-10-26 17:52:28 +0700

    Add enemy and enemycontrol

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public Transform[] patrolPoints;
+	public float moveSpeed;
+	private int currentPoint;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = patrolPoints [0].position;
+		currentPoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentPoint >= patrolPoints.Length) {
+			currentPoint = 0;
+		}
+		if (transform.position == patrolPoints [currentPoint].position) {
+			currentPoint++;
+		}
+		transform.position = Vector3.MoveTowards (transform.position, patrolPoints [currentPoint].position,
+			moveSpeed * Time.deltaTime);
+	}
+}
