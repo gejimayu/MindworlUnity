6fd792be62b30736f8be96ecb83e15090edcb45e
gejimayu <gianffh@gmail.com>
2017-10-26 21:55:09 +0700

    Add score and win/lose condition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
 	public Rigidbody rb;
 	public GameObject deathParticles;
+	public Text lifebar;
+	public Text notification;
 	private Vector3 spawn;
 	private Vector3 input;
+	private int lives;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		spawn = transform.position;
+		lives = 5;
 	}
 	
 	// Update is called once per frame
@@ -25,8 +30,15 @@ public class PlayerController : MonoBehaviour {
 	void OnCollisionEnter(Collision other){
 		if (other.transform.tag == "Enemy") {
 			Instantiate (deathParticles, transform.position, Quaternion.identity);
+			lives = lives - 1;
+
+			if (lives == 0) 
+				notification.text = "You Lose !";
+			
 			transform.position = spawn;
+			lifebar.text = "Lives : " + lives.ToString ();
 		} else if (other.transform.tag == "Goal") {
+			notification.text = "You Win !";
 		}
 	}
 }
