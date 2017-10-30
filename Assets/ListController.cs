2097790a863d47b0040323365a774d0db40905a4
gejimayu <gianffh@gmail.com>
2017-10-30 19:30:09 +0700

    Make items dynamic and add image panel

--- Assets/ListController.cs
+++ Assets/ListController.cs
@@ -2,23 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using SimpleJSON;
+
 public class ListController : MonoBehaviour {
 
 	public GameObject ContentPanel;
 	public GameObject ListItemPrefab;
-
-	ArrayList Items;
+	public string textsUrl = "https://mindworld-4964e.firebaseio.com/text.json";
+	public JSONObject textsNode;
+	public JSONObject imagesNode;
+	ArrayList ItemsText;
 
 	// Use this for initialization
-	void Start () {
-		Items = new ArrayList () {
-			new Barang ("asd.txt", "asem"),
-			new Barang ("Abc.txt", "aso"),
-			new Barang ("adem.txt", "abc")
-		};
+	IEnumerator Start () {
+		//Items = new ArrayList () {
+			//new Barang ("asd.txt", "asem"),
+			//new Barang ("Abc.txt", "aso"),
+			//new Barang ("adem.txt", "abc")
+		//};
 
-		foreach(Barang temp in Items) {
-			Debug.Log (temp.txt);
+		ItemsText = new ArrayList ();
+		WWW www = new WWW(textsUrl);
+		yield return www;
+
+		textsNode = (JSONObject) JSON.Parse (www.text);
+
+		foreach (string key in textsNode.Keys) {
+			Debug.Log (textsNode[key]["title"]);
+			WWW temp = new WWW (textsNode [key] ["url"]);
+			yield return temp;
+			ItemsText.Add(new BarangTxt(textsNode[key]["title"], temp.text));
+		}
+
+		foreach(BarangTxt temp in ItemsText) {
+			//Debug.Log (temp.txt);
 			GameObject newItem = Instantiate (ListItemPrefab) as GameObject;
 			ListItemController controller = newItem.GetComponent<ListItemController>();
 			controller.content.text = temp.txt;
---
+++ Assets/ListController2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using SimpleJSON;
+
+public class ListController2 : MonoBehaviour {
+
+	public GameObject ContentPanel;
+	public GameObject ListItemPrefab;
+	public string imagesUrl = "https://mindworld-4964e.firebaseio.com/image.json";
+	public JSONObject imagesNode;
+	ArrayList ItemsImage;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		//Items = new ArrayList () {
+		//new Barang ("asd.txt", "asem"),
+		//new Barang ("Abc.txt", "aso"),
+		//new Barang ("adem.txt", "abc")
+		//};
+
+		ItemsImage = new ArrayList ();
+		WWW www = new WWW(imagesUrl);
+		yield return www;
+
+		imagesNode = (JSONObject) JSON.Parse (www.text);
+
+		foreach (string key in imagesNode.Keys) {
+			Debug.Log (imagesNode[key]["name"]);
+			WWW temp = new WWW (imagesNode [key] ["url"]);
+			yield return temp;
+			//convert image to sprite
+			Texture2D texture = new Texture2D (temp.texture.width, temp.texture.height, TextureFormat.DXT1, false);
+			temp.LoadImageIntoTexture (texture);
+			Rect rec = new Rect (0, 0, texture.width, texture.height);
+			Sprite spriteToUse = Sprite.Create (texture, rec, new Vector2 (0.5f, 0.5f), 100);
+
+			ItemsImage.Add(new BarangImg(imagesNode[key]["name"], spriteToUse));
+		}
+
+		foreach(BarangImg temp in ItemsImage) {
+			//Debug.Log (temp.txt);
+			GameObject newItem = Instantiate (ListItemPrefab) as GameObject;
+			ListItemController2 controller = newItem.GetComponent<ListItemController2>();
+			controller.content.sprite = temp.img;
+			controller.fname.text = temp.filename;
+			newItem.transform.SetParent (ContentPanel.transform, false);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
