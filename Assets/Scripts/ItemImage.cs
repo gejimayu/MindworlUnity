201d9b69b7b2d995fd43fcb70c7b6aa86e18964c
Albertus Djauhari Djohan <albertusdjauharidjohan@gmail.com>
2017-10-30 15:28:41 +0700

    Add item script

---
+++ Assets/Scripts/ItemImage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarangImg {
+
+	public string filename;
+	public Sprite img;
+
+	public BarangImg(string fn, Sprite img) {
+		filename = fn;
+		this.img = img;
+	}
+}
