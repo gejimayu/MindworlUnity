01cfc0380db7b320695712247478cf31196b007f
gejimayu <gianffh@gmail.com>
2017-10-30 22:26:21 +0700

    Moved scrips and prefabs

---
+++ Assets/Scripts/ItemText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarangTxt {
+
+	public string filename, txt;
+	
+	public BarangTxt(string fn, string content) {
+		filename = fn;
+		txt = content;
+	}
+}
