a2a92fb0176e0106f6271119a78fe81fb331ec60
gejimayu <gianffh@gmail.com>
2017-10-30 17:56:21 +0700

    Add scrollable list of text panel

---
+++ Assets/ItemText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemText : MonoBehaviour {
+
+	public string content;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
